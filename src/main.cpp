c8dda7cc06f0df5e3745d9453c28468617afe674
Camille GAUTHIER <al.gauthier@student.isartdigital.com>
2023-06-05 09:11:43 +0200

    Code clean

--- src/main.cpp
+++ src/main.cpp
@@ -12,34 +12,41 @@
 std::string GetDownloadURL()
 {
 	IStream* fileStream;
-	HRESULT downloadResult = URLOpenBlockingStream(NULL, L"https://api.github.com/repos/R2Northstar/northstar/releases/latest", &fileStream, 0, NULL);
 
-	STATSTG streamStats{};
+	DWORD byteCount;
+	DWORD stringSize;
 
-	fileStream->Stat(&streamStats, STATFLAG_DEFAULT);
-
-	DWORD stringSize = streamStats.cbSize.LowPart;
+	STATSTG streamStats;
 
 	std::string resultStr;
 
+	memset(&streamStats, 0, sizeof(STATSTG));
+
+	URLOpenBlockingStream(NULL, L"https://api.github.com/repos/R2Northstar/northstar/releases/latest", &fileStream, 0, NULL);
+
+	fileStream->Stat(&streamStats, STATFLAG_DEFAULT);
+
+	stringSize = streamStats.cbSize.LowPart;
+
 	resultStr.reserve(stringSize);
 	resultStr.resize(stringSize);
-
-	DWORD byteCount;
 
 	fileStream->Read(&resultStr[0], stringSize, &byteCount);
 	fileStream->Release();
 
 	constexpr int searchStrSize = 23;
-	size_t urlOffset = resultStr.find("\"browser_download_url\":") + searchStrSize + 1; //+ 1 to remove the extra " character
-	size_t urlStrSize = (resultStr.find(".zip\"", urlOffset) - urlOffset) + 4; // 4 -> ".zip"
+
+	///Here we are looking for the "browser_download_url" entry to parse the direct url to the archive.
+	///It should look something like : 
+	/// -	"browser_download_url": "https://github.com/R2Northstar/Northstar/releases/download/v1.14.2/Northstar.release.v1.14.2.zip"
+
+	size_t urlOffset	= resultStr.find("\"browser_download_url\":") + searchStrSize + 1;	//+ 1 to remove the extra " character
+	size_t urlStrSize	= (resultStr.find(".zip\"", urlOffset) - urlOffset) + 4;			// 4 -> ".zip"
 
 	return resultStr.substr(urlOffset, urlStrSize);
 }
 
 ///Decompress a zip archive to the working directory
-//TODO : 
-// - Better memory management
 void DecompressZIPFolder(mz_zip_archive* pArchive)
 {
 	if (!pArchive) return;
@@ -65,39 +72,57 @@ void DecompressZIPFolder(mz_zip_archive* pArchive)
 /// Download and write the zip file from pFileURL url onto the disk.
 void DownloadAndExtractZip(const std::string& pFileURL)
 {
-	std::wstring wURL(pFileURL.begin(), pFileURL.end());
+	std::wstring wURL;
 
 	IStream* fileStream;
-	HRESULT downloadResult = URLOpenBlockingStream(NULL, wURL.c_str(), &fileStream, 0, NULL);
+	HRESULT downloadResult;
+	STATSTG streamStats;
+		
+	DWORD byteCount;
+	DWORD fileSize;
+
+	HANDLE zipHandle;
+
+	std::vector<char> fileBuffer;
+
+	mz_zip_archive archive;
+
+	//Download file
+
+	wURL = std::wstring(pFileURL.begin(), pFileURL.end());
+
+	downloadResult = URLOpenBlockingStream(NULL, wURL.c_str(), &fileStream, 0, NULL);
 
 	if (!SUCCEEDED(downloadResult))
 		return;
 
-	STATSTG streamStats{};
+	memset(&streamStats, 0, sizeof(STATSTG));
+	memset(&archive,	 0,	sizeof(mz_zip_archive));
+
+	//Get data
 
 	fileStream->Stat(&streamStats, STATFLAG_DEFAULT);
 
-	DWORD byteCount;
-	DWORD fileSize = streamStats.cbSize.LowPart;
+	fileSize = streamStats.cbSize.LowPart;
 
-	std::vector<char> fileBuffer;
 	fileBuffer.reserve(fileSize);
 
 	fileStream->Read(fileBuffer.data(), fileSize, &byteCount);
-
 	fileStream->Release();
+
+	//Write file
 
 	PathStripPath(&wURL[0]);
 
 	std::string fileName(wURL.begin(), wURL.end());
 
-	HANDLE zipHandle = CreateFileA(&fileName[0], GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	zipHandle = CreateFileA(&fileName[0], GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 
 	WriteFile(zipHandle, fileBuffer.data(), fileSize, &byteCount, NULL);
 
 	CloseHandle(zipHandle);
 
-	mz_zip_archive archive{};
+	//Decompress archive
 
 	mz_zip_reader_init_file(&archive, fileName.c_str(), 0);
 
