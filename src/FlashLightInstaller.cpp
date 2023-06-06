c34a936695fe479a18ef8902f2fe3042ee0ab1a9
Camille GAUTHIER <al.gauthier@student.isartdigital.com>
2023-06-06 14:40:32 +0200

    Code refactor. Preparing the implementation of a GUI.

---
+++ src/FlashLightInstaller.cpp
@@ -0,0 +1,142 @@
+#include <Windows.h>
+#include <urlmon.h>
+#include <shlwapi.h>
+
+#include <string>
+
+#include <vector>
+
+#include "miniz/miniz.h"
+
+#include "FlashLightInstaller.hpp"
+
+void FlashlightInstaller::InitWindow(HINSTANCE hInstance, int nCmdShow)
+{
+
+}
+
+///Run the entire installation process : Parse the url to the latest release, then download and extract the archive.
+void FlashlightInstaller::RunInstallation()
+{
+	this->GetDownloadURL(&mArchive);
+	this->DownloadArchive(&mArchive);
+	this->ExtractArchive(&mArchive);
+}
+
+/// Use GitHub api to retrive the direct link to the latest release of NorthStar.
+void FlashlightInstaller::GetDownloadURL(NorthStarArchive* pArchive)	const
+{
+	IStream* fileStream;
+
+	DWORD byteCount;
+	DWORD stringSize;
+
+	STATSTG streamStats;
+
+	std::string apiStr;
+
+	memset(&streamStats, 0, sizeof(STATSTG));
+
+	URLOpenBlockingStream(NULL, L"https://api.github.com/repos/R2Northstar/northstar/releases/latest", &fileStream, 0, NULL);
+
+	fileStream->Stat(&streamStats, STATFLAG_DEFAULT);
+
+	stringSize = streamStats.cbSize.LowPart;
+
+	apiStr.reserve(stringSize);
+	apiStr.resize(stringSize);
+
+	fileStream->Read(&apiStr[0], stringSize, &byteCount);
+	fileStream->Release();
+
+	///Here we are looking for the "browser_download_url" entry to parse the direct url to the archive.
+	///It should look something like : 
+	/// -	"browser_download_url": "https://github.com/R2Northstar/Northstar/releases/download/v1.14.2/Northstar.release.v1.14.2.zip"
+
+	constexpr int searchStrSize = 23;
+
+	size_t urlOffset = apiStr.find("\"browser_download_url\":") + searchStrSize + 1;	//+ 1 to remove the extra " character
+	size_t urlStrSize = (apiStr.find(".zip\"", urlOffset) - urlOffset) + 4;				// 4 -> ".zip"
+
+	pArchive->URL = apiStr.substr(urlOffset, urlStrSize);
+}
+
+/// Download and write the zip file from pFileURL url onto the disk.
+void FlashlightInstaller::DownloadArchive(NorthStarArchive* pArchive)	const
+{
+	std::wstring wURL;
+
+	IStream* fileStream;
+	HRESULT downloadResult;
+	STATSTG streamStats;
+
+	DWORD byteCount;
+	DWORD fileSize;
+
+	HANDLE zipHandle;
+
+	std::vector<char> fileBuffer;
+
+	memset(&streamStats, 0, sizeof(STATSTG));
+
+	//Download file
+
+	wURL = std::wstring(pArchive->URL.begin(), pArchive->URL.end());
+
+	downloadResult = URLOpenBlockingStream(NULL, wURL.c_str(), &fileStream, 0, NULL);
+
+	if (!SUCCEEDED(downloadResult))
+		return;
+
+	PathStripPath(&wURL[0]);
+	pArchive->path = std::string(wURL.begin(), wURL.end());
+
+	{
+		fileStream->Stat(&streamStats, STATFLAG_DEFAULT);
+
+		fileSize = streamStats.cbSize.LowPart;
+
+		fileBuffer.reserve(fileSize);
+
+		fileStream->Read(fileBuffer.data(), fileSize, &byteCount);
+		fileStream->Release();
+	}
+
+	zipHandle = CreateFileA(&pArchive->path[0], GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	WriteFile(zipHandle, fileBuffer.data(), fileSize, &byteCount, NULL);
+
+	CloseHandle(zipHandle);
+}
+
+///Extract the archive to the disk
+void FlashlightInstaller::ExtractArchive(NorthStarArchive* pArchive)	const
+{
+	mz_zip_archive archive;
+
+	memset(&archive, 0, sizeof(mz_zip_archive));
+
+	mz_zip_reader_init_file(&archive, pArchive->path.c_str(), 0);
+
+	const int fileCount = mz_zip_reader_get_num_files(&archive);
+
+	if (fileCount <= 0)
+		return;
+
+	mz_zip_archive_file_stat stats;
+
+	for (int i = 0; i < fileCount; i++)
+	{
+		memset(&stats, 0, sizeof(mz_zip_archive_file_stat));
+		mz_zip_reader_file_stat(&archive, i, &stats);
+
+		const bool isDirectory = mz_zip_reader_is_file_a_directory(&archive, i);
+
+		if (isDirectory)
+			CreateDirectoryA(stats.m_filename, NULL);
+		else
+			mz_zip_reader_extract_to_file(&archive, i, stats.m_filename, 0);
+	}
+
+	mz_zip_reader_end(&archive);
+}
---
+++ src/FlashLightInstaller.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <Windows.h>
+
+#include <string>
+
+class FlashlightInstaller
+{
+public:
+	FlashlightInstaller() = default;
+	~FlashlightInstaller() = default;
+
+private:
+
+	typedef struct FlashlightWindow_
+	{
+		HWND hwnd		= NULL;
+		LPCWSTR title	= L"FlashlightInstaller";
+	}FlashlightWindow;
+
+	typedef struct NorthStarArchive_
+	{
+		std::string URL		= "";
+		std::string path	= "";
+	}NorthStarArchive;
+
+	FlashlightWindow mWindow;
+	NorthStarArchive mArchive;
+
+public:
+	///Optional function : Init installer GUI.
+	void InitWindow(HINSTANCE hInstance, int nCmdShow);
+
+	///Run the entire installation process : Parse the url to the latest release, then download and extract the archive.
+	void RunInstallation();
+
+private:
+
+	/// Use GitHub api to retrive the direct link to the latest release of NorthStar.
+	void GetDownloadURL		(NorthStarArchive *pArchive)	const;
+
+	/// Download and write the archive from the url onto the disk.
+	void DownloadArchive	(NorthStarArchive *pArchive)	const;
+
+	///Extract the archive to the disk
+	void ExtractArchive		(NorthStarArchive *pArchive)	const;
+};
